727fa623287616b12c7237d4eb78ee910a773e4d
dgomez40 <dgomez40@terpmail.umd.edu>
2024-03-14 10:21:09 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,13 +2,9 @@ print('hello world')
 
 print ('Sahaj Patel was here')
 
-<<<<<<< HEAD
+print('Douglas Gomez')
 
+print('Carl')
 
 
 print ('Henry Nguyen is here.')
-=======
-print ('Carl')
-
-print ('Douglas Gomez')
->>>>>>> e14292a89f53b7f5886a2e7dcd732823d1492041
