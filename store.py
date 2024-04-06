0d0fb513ac4d53bed3bddcf0960a08eed3a60f03
sahajp-11 <125601575+sahajp-11@users.noreply.github.com>
2024-04-06 13:43:36 -0400

    created store.py

---
+++ store.py
@@ -0,0 +1,91 @@
+
+
+import re
+
+
+class Categories:
+    
+    
+    def __init__():
+        
+    def category_name():
+        
+    def subcategories():
+        
+    def url():
+        
+
+class Computers:
+    
+    def __init__():
+        
+    def item_name():
+        
+    def description():
+        
+    def category():
+        
+    def price():
+        
+    def reviews():
+    
+    def stars():
+        
+        
+
+class Phones:
+    
+    def __init__():
+    
+    def item_name():
+    
+    def features():
+    
+    def category():
+        
+    def price():
+        
+    def reviews():
+        
+    def stars():   
+        
+        
+        
+    
+def get_categories():
+    
+    
+    
+    
+# def categories_to_csv():    
+
+
+
+
+def get_items():
+    
+    
+    
+
+# def items_to_csv():
+    
+    
+    
+        
+    
+    
+    
+    
+        
+    
+
+
+
+
+
+
+
+
+
+
+
