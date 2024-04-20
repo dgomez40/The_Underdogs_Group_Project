d684f2bc293e93f0c008e8b033e330559365bbf4
Josiah Altschuler <yaltschuler@gmail.com>
2024-04-20 14:23:12 -0400

    Add updated categories regex

--- store.py
+++ store.py
@@ -62,10 +62,20 @@ ctx.verify_mode = ssl.CERT_NONE
     
 def get_categories(url):
     html = urllib.request.urlopen(url, context=ctx).read().decode('utf-8')
-    categories_array = []
+    urls = []
     index = 0
-    regex = re.findall("(?<=href=\")/test-sites/e-commerce/allinone(?!/product/)[^\"]*(?=\")", html)
-    print(str(regex))
+    partial_urls = re.findall("(?<=href=\")/test-sites/e-commerce/allinone(?!/product/)[^\"]*(?=\")", html)
+    category_names = re.findall("(?<=class=\"nav-link\">).*|(?<=nav-link \">\n)\s*(.*)(?=\n)")
+    for address in partial_urls:
+        # print(url)
+        urls.append(f"https://webscraper.io{url}")
+    for address in urls:
+        if address != url:
+            sub_html = urllib.request.urlopen(address, context=ctx).read().decode('utf-8')
+            sub_urls = re.findall("(?<=href=\")/test-sites/e-commerce/allinone(?!/product/)[^\"]*(?=\")", sub_html)
+            sub_names = re.findall("(?<=class=\"nav-link\">).*|(?<=nav-link \">\n)\s*(.*)(?=\n)")
+            
+        
     # if regex is not None:
     #     category_url = f"https://webscraper.io + {regex.string}"
     #     print(category_url)        
@@ -78,24 +88,6 @@ def get_categories(url):
     
 # # def categories_to_csv():    
 
-def get_items(url):
-    html_code_home_page = urllib.request.urlopen(url, context=ctx).read().decode('utf-8')
-    item_name = re.findall('(?<=title=\").*(?=\")', html_code_home_page)
-    item_price = re.findall('price float-end card-title pull-right\".(.*)<', html_code_home_page)
-    item_description = re.findall('description card-text\".(.*)<', html_code_home_page)
-    item_stars = re.findall('data-rating=\"(.*)\"', html_code_home_page)
-    item_review_count = re.findall('review-count float-end\">(.*)<', html_code_home_page)
-    #item_color = re.findall('class=\"dropdown-item\" value=\"(.*)\"', html_code_home_page)
-
-    print(str(item_name))
-    print(str(item_price))
-    print(str(item_description))
-    print(str(item_stars))
-    print(str(item_review_count))
-    #print(str(item_color))
-    
-     
-    
     
 
 # # def items_to_csv():
