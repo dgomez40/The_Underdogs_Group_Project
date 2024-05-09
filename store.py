ef7982cb6fd115c5460d004a64938eb2927071f1
sahajp-11 <125601575+sahajp-11@users.noreply.github.com>
2024-05-08 22:01:56 -0400

    store script

--- store.py
+++ store.py
@@ -2,6 +2,18 @@ import scraper as  sc
 import pandas as pd
 import tabulate as tb
 
+df = pd.read_csv('output.csv')
+
+price = df['Price'].str.replace('$', '').astype(float)
+
+category = input('Choose your category between Phones and Computers: ').capitalize()
+
+min_price = int(input('Enter your minimum price: '))
+max_price = int(input('Enter your maximum price: '))
+
+new_df = df[(df['Category Name'] == category) & (price >= min_price) & (price <= max_price)]
+        
+
 dataframe = sc.get_items(sc.category_dict)
 
 def store():
