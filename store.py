56dee71acf6144ad8669c80e1541e1cad2db2b65
sahajp-11 <125601575+sahajp-11@users.noreply.github.com>
2024-05-09 14:16:51 -0400

    store completed

--- store.py
+++ store.py
@@ -1,75 +1,85 @@
 import scraper as  sc
 import tabulate as tb
 
-df = sc.get_items("https://webscraper.io/test-sites/e-commerce/allinone")
+def manipulate_df():
+    
+    df = sc.get_items("https://webscraper.io/test-sites/e-commerce/allinone")
 
-df['Price'] = df['Price'].str.replace('$', '').astype(float)
+    df['Price'] = df['Price'].str.replace('$', '').astype(float)
 
-category = input('Choose your category between Phones and Computers: ').capitalize()
+    category = input('Choose your category between Phones and Computers: ').capitalize()
 
-min_price = float(input('Enter your minimum price: '))
-max_price = float(input('Enter your maximum price: '))
+    min_price = float(input('Enter your minimum price: '))
+    max_price = float(input('Enter your maximum price: '))
 
-new_df = df[(df['Category Name'] == category) & (df['Price'] >= min_price) & (df['Price'] <= max_price)]
-new_df = new_df.reset_index(drop=True)
+    new_df = df[(df['Category Name'] == category) & (df['Price'] >= min_price) & (df['Price'] <= max_price)]
+    return new_df.reset_index(drop=True)
+    
 
 def add_cart(cart, item):
     cart.append(item)
 
-cart = []
-
-if len(new_df) != 0:
+def store():
     
-    print('Items in stock based on your price range:')
-    for num, item in new_df.iterrows():
-        print(f"{num + 1}. {item['Item Name']} ${item['Price']}")
+    df = manipulate_df()    
+    cart = []
 
-    while True:
+    if len(df) != 0:
         
-        choice = int(input('Enter the item number of the item you want to see details for: '))
+        print('Items in stock based on your price range:')
+        for num, item in df.iterrows():
+            print(f"{num + 1}. {item['Item Name']} ${item['Price']}")
+
+        while True:
+            
+            choice = int(input('Enter the item number of the item you want to see details for: '))
+            
+            if choice > 0 and choice <= len(df):
+                
+                item_selection = df.iloc[choice - 1]
+                
+                print(f"Item Name: {item_selection['Item Name']}")
+                print(f"Item URL: {item_selection['Item URL']}")
+                print(f"Price: ${item_selection['Price']}")
+                print(f"Description: {item_selection['Description']}")
+                print(f"Stars: {item_selection['Stars']}")
+                print(f"Reviews: {item_selection['Reviews']}")
+                print(f"Variants: {item_selection['Variants']}")
+                print(f"Colors: {item_selection['Colors']}")
+                
+                ask_to_add = input('Would you like to add this item to the cart? ').lower()
+                
+                if ask_to_add == 'yes':
+                    
+                    add_cart(cart, item_selection)
+                    keep_shopping = input('Would you like to keep shopping? ')
+                    
+                    if keep_shopping == 'no':
+                        break
+                    
+            else:   
+                print("That is an invalid item number, please enter one that is within the range.")
+                
+
+    else:
+        print("There is nothing in stock that fits within your price range.")
+        quit
+
+
+    empty = True
+
+
+    if cart is not empty:
+        total_price = sum(item['Price'] for item in cart)
+        print(f'Your total is: ${total_price}')
+    else:
+        print("You did not select anything.")
         
-        if choice > 0 and choice <= len(new_df):
+if __name__ == '__main__':
+    
+    store()
+
+
             
-            item_selection = new_df.iloc[choice - 1]
-            
-            print(f"Item Name: {item_selection['Item Name']}")
-            print(f"Item URL: {item_selection['Item URL']}")
-            print(f"Price: ${item_selection['Price']}")
-            print(f"Description: {item_selection['Description']}")
-            print(f"Stars: {item_selection['Stars']}")
-            print(f"Reviews: {item_selection['Reviews']}")
-            print(f"Variants: {item_selection['Variants']}")
-            print(f"Colors: {item_selection['Colors']}")
-            
-            ask_to_add = input('Would you like to add this item to the cart? ').lower()
-            
-            if ask_to_add == 'yes':
-                
-                add_cart(cart, item_selection)
-                keep_shopping = input('Would you like to keep shopping? ')
-                
-                if keep_shopping == 'no':
-                    break
-                
-        else:   
-            print("That is an invalid item number, please enter one that is within the range.")
-            
-
-else:
-    print("There is nothing in stock that fits within your price range.")
-    quit
-
-
-empty = True
-
-
-if cart is not empty:
-    total_price = sum(item['Price'] for item in cart)
-    print(f'Your total is: ${total_price}')
-else:
-    print("You did not select anything.")
-
-
-        
 
 
