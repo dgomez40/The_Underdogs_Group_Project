596280008fafe8f560b6aec3d12cd7f03de2a9f9
sahajp-11 <125601575+sahajp-11@users.noreply.github.com>
2024-05-09 03:02:36 -0400

    added a little more to store.py

--- store.py
+++ store.py
@@ -2,19 +2,40 @@ import scraper as  sc
 import pandas as pd
 import tabulate as tb
 
+
 df = pd.read_csv('output.csv')
 
-price = df['Price'].str.replace('$', '').astype(float)
+df['Price'] = df['Price'].str.replace('$', '').astype(float)
 
 category = input('Choose your category between Phones and Computers: ').capitalize()
 
-min_price = int(input('Enter your minimum price: '))
-max_price = int(input('Enter your maximum price: '))
+min_price = float(input('Enter your minimum price: '))
+max_price = float(input('Enter your maximum price: '))
 
-new_df = df[(df['Category Name'] == category) & (price >= min_price) & (price <= max_price)]
+new_df = df[(df['Category Name'] == category) & (df['Price'] >= min_price) & (df['Price'] <= max_price)]
+new_df = new_df.reset_index(drop=True)
+
+
+def add_to_cart(cart, product):
+    cart.append(product)
+    
+cart = []
+
+
+
+if len(new_df) != 0:
+    
+    print('What is in stock based on your price range:')
+    for num, item in new_df.iterrows():
+        print(f"{num + 1}. {item['Item Name']} ${item['Price']}")
+        
+else:
+    quit
+    
+
         
 
-dataframe = sc.get_items(sc.category_dict)
+# dataframe = sc.get_items(sc.category_dict)
 
-def store():
-    pass
+# def store():
+#     pass
